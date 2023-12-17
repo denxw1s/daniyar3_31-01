9f802080a1b58131b685a12d9514c4765b20b9ab
denxw1s <151997588+denxw1s@users.noreply.github.com>
2023-12-17 16:58:37 +0600

    initial commit

---
+++ lesson1/src/Companent.jsx
@@ -0,0 +1,16 @@
+
+export function Component() {
+    return (
+        <div>
+            Component
+        </div>
+    );
+}
+
+export function Component2() {
+    return (
+        <div>
+            Component
+        </div>
+    );
+}
---
+++ lesson1/src/assets/Footer.js
@@ -0,0 +1,7 @@
+// function addFooter(content) {
+//     let footer = document.createElement('footer');
+//     footer.textContent = content;
+//     document.body.appendChild(footer);
+// }
+//
+// addFooter('Hello, my name is Daniar')
